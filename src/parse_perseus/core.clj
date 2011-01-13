ee1769e91c392d64aad8b583172a245f39704841
William Roe <william.roe@migcan.com>
2011-01-13 17:53:51 +0000

    Initial commit

---
+++ src/parse_perseus/core.clj
@@ -0,0 +1,1 @@
+(ns parse_perseus.core)
---
+++ test/parse_perseus/test/core.clj
@@ -0,0 +1,6 @@
+(ns parse_perseus.test.core
+  (:use [parse_perseus.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
