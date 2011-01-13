8594885e979e46b9689fecbd7d6ef4dc0e0fa4d7
William Roe <william.roe@migcan.com>
2011-01-13 18:00:49 +0000

    swank dev deps

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject parse_perseus "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "For parsing Perseus files - http://www.perseus.tufts.edu/hopper/opensource/download"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
