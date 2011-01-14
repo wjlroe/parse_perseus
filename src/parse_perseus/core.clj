a71db94f797583605740667e4102afe07e318ef1
William Roe <willroe@gmail.com>
2011-01-14 00:28:14 +0000

    Added some definitions for testing encoding... Beta Code! Yeah.

--- src/parse_perseus/core.clj
+++ src/parse_perseus/core.clj
@@ -1,1 +1,9 @@
 (ns parse_perseus.core)
+
+;; Beta Code = bc = funny combinations of chars
+
+(def iliad_first_line_bc "mh=nin a)/eide qea\ *phlhi+a/dew *)axilh=os")
+(def odyssey_first_line_bc "a)/ndra moi e)/nnepe, mou=sa, polu/tropon, o(\s ma/la polla\\")
+(def odyssey_first_line_gk "ἄνδρα μοι ἔννεπε, μοῦσα, πολύτροπον, ὃς μάλα πολλὰ")
+
+
