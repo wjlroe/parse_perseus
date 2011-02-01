7f100a71b6e28673798c3ee099f2bda2e5270bd8
William Roe <willroe@gmail.com>
2011-02-01 21:42:11 +0000

    Add utf-8 options to leiningen project file so output works properly

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [fnparse "2.2.7"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+  :dev-dependencies [[swank-clojure "1.2.0"]]
+  :jvm-opts ["-Dfile.encoding=utf-8"])
