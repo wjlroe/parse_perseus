55d4bbbe2101f6eeecc39b24b49e3d34b86b466f
William Roe <willroe@gmail.com>
2013-11-01 15:49:22 +0000

    Clojure 1.5.1 gives us the lovely some-> macro

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject parse_perseus "1.0.0-SNAPSHOT"
   :description "For parsing Perseus files - http://www.perseus.tufts.edu/hopper/opensource/download"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [factual/fnparse "2.3.0"]
                  [org.clojure/data.xml "0.0.7"]
                  [compojure "1.1.5"]
