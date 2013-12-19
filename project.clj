1792962d5d81b7fcb9cdbbefcd30cea50fb37789
William Roe <willroe@gmail.com>
2013-12-19 18:00:43 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
                  [factual/fnparse "2.3.0"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojars.wjlroe/hiccup "1.0.5-SNAPSHOT"]
-                 [cheshire "5.2.0"]
                  [environ "0.4.0"]
                  [selmer "0.5.4"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.52"]]}}
+  :profiles {:dev {:dependencies [[expectations "1.4.56"]]}}
   :jvm-opts ["-Dfile.encoding=utf-8"]
   :main parse_perseus.book)
