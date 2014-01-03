7ac6b76dc7b51d5edbc2f36996c29ad88eb88cd0
William Roe <willroe@gmail.com>
2014-01-03 18:15:20 +0000

    Extract commonly used function
    This used to be part of clojure-contrib

--- src/parse_perseus/book.clj
+++ src/parse_perseus/book.clj
@@ -1,6 +1,7 @@
 (ns parse_perseus.book
   (:use
     parse_perseus.betacode
+    parse_perseus.utils
     clojure.xml
     [clojure.java.io :only [make-parents file copy delete-file]])
   (:require
@@ -259,16 +260,6 @@
         (debug "About to write-file: " thefilename)
         (write-file (file (ebook-dir book) thefilename)
                     (contents-fun book))))))
-
-(defn delete-file-recursively
-  "Delete file f. If it's a directory, recursively delete all its contents.
-  Raise an exception if any deletion fails unless silently is true."
-  [f & [silently]]
-  (let [f (file f)]
-    (if (.isDirectory f)
-      (doseq [child (.listFiles f)]
-        (delete-file-recursively child silently)))
-    (delete-file f silently)))
 
 (defn generate-book [book]
   (try
