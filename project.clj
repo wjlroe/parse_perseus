ef4f140d5b1ea5fc3dc72850c38cc4b5016e07f1
William Roe <willroe@gmail.com>
2014-03-25 15:01:38 +0000

    Clojure 1.6 released! Woo hoo!!

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject parse_perseus "2.0.0-SNAPSHOT"
   :description "For parsing Perseus files - http://www.perseus.tufts.edu/hopper/opensource/download"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [factual/fnparse "2.3.0"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojars.wjlroe/hiccup "1.0.5-SNAPSHOT"]
