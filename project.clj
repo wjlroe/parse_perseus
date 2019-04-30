2ee378291fba2cc00313980d4b37141536fd8e6e
William Roe <git@wjlr.org.uk>
2019-04-30 16:19:05 +0100

    Clojure update

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject parse_perseus "2.0.0-SNAPSHOT"
   :description "For parsing Perseus files - http://www.perseus.tufts.edu/hopper/opensource/download"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [factual/fnparse "2.3.0"]
-                 [org.clojure/data.xml "0.0.7"]
+                 [org.clojure/data.xml "0.0.8"]
                  [org.clojars.wjlroe/hiccup "1.0.5-SNAPSHOT"]
-                 [environ "0.4.0"]
-                 [selmer "0.5.9"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.56"]]}}
+                 [environ "1.0.1"]
+                 [selmer "0.9.4"]]
+  :profiles {:dev {:dependencies [[expectations "1.4.56"]
+                                  [erajure "0.0.4"]]}}
   :plugins [[lein-expectations "0.0.8"]]
   :jvm-opts ["-Dfile.encoding=utf-8" "-Xms128m"]
   :main parse_perseus.book)
